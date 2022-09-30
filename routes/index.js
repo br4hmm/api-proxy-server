f32529cc72ac36a1025b0be364bfd8e289f2adc5
Ibrahim <ibrahimghali211@gmail.com>
2022-10-01 00:15:53 +0300

    request from the server

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
+const needle = require('needle');
 const { Router } = require('express');
+
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ success: true });
+router.get('/', async (req, res) => {
+  try {
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+    });
+
+    const apiRes = await needle('get', `${API_BASE_URL}?${params}`);
+    const data = apiRes.body;
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
 module.exports = router;
