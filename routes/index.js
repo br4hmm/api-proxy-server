a59e753c32d98057bb91cf8208c38fa9dbb21ffe
Ibrahim <ibrahimghali211@gmail.com>
2022-10-01 00:35:19 +0300

    add api caching

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 const needle = require('needle');
 const url = require('url');
+const apicache = require('apicache');
+
 const { Router } = require('express');
+const router = Router();
 
 const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
-const router = Router();
+let cache = apicache.middleware;
 
-router.get('/', async (req, res) => {
+router.get('/', cache('2 minutes'), async (req, res) => {
   try {
     const params = new URLSearchParams({
       [API_KEY_NAME]: API_KEY_VALUE,
